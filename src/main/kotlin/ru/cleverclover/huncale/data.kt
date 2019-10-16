d0268c4e415207868e47f68993d61354398c85f6
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-10-16 12:39:23 +0300

    delete redundant code container

--- src/main/kotlin/ru/cleverclover/huncale/data.kt
+++ src/main/kotlin/ru/cleverclover/huncale/data.kt
@@ -2,8 +2,28 @@ package ru.cleverclover.huncale
 
 import org.json.simple.JSONArray
 import org.json.simple.JSONObject
+import org.json.simple.parser.JSONParser
 import ru.cleverclover.metacalendar.Cashed
 import ru.cleverclover.metacalendar.PeriodFromRangeDefinition
+import java.io.InputStreamReader
+
+internal class JsonData {
+
+    fun targets(): Targets {
+        val categories: Categories = categories() ?: return NoTargets()
+        return targets(categories) ?: return NoTargets()
+    }
+
+    private fun categories() =
+            JsonData::class.java.classLoader.getResourceAsStream("data/category.json")?.use {
+                Categories(JSONParser().parse(InputStreamReader(it)) as JSONArray)
+            }
+
+    private fun targets(categories: Categories) =
+            JsonData::class.java.classLoader.getResourceAsStream("data/resource.json")?.use {
+                Targets(JSONParser().parse(InputStreamReader(it)) as JSONArray, categories)
+            }
+}
 
 internal class Categories(source: JSONArray) {
     private val categories = Cashed(source) { array -> array.map { Category(it as JSONObject) } }
@@ -28,7 +48,7 @@ internal open class Targets(source: JSONArray, val categories: Categories) {
     fun targets() = targets.get()
 }
 
-internal class NoTargets() : Targets(JSONArray(), Categories(JSONArray()))
+internal class NoTargets : Targets(JSONArray(), Categories(JSONArray()))
 
 internal class Target(private val source: JSONObject, private val categoryFromSource: (source: JSONObject) -> Category) {
     private val restrictions = Cashed(source["restrictions"] as JSONArray)
