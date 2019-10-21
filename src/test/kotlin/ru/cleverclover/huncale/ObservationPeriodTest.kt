7269f082d225dc5e135334b581a60fff6b34ab0f
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2019-10-21 20:26:17 +0300

    test ObservationPeriod properly

--- src/test/kotlin/ru/cleverclover/huncale/ObservationPeriodTest.kt
+++ src/test/kotlin/ru/cleverclover/huncale/ObservationPeriodTest.kt
@@ -1,41 +1,88 @@
 package ru.cleverclover.huncale
 
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
-import org.junit.Test
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import ru.cleverclover.huncale.timemachine.ObservationPeriod
 import java.time.LocalDate
 import java.time.Month
 
 class ObservationPeriodTest {
     @Test
-    fun singleYear() {
-        val period = ObservationPeriod(LocalDate.of(2019, Month.FEBRUARY, 2),
-                LocalDate.of(2019, Month.APRIL, 12))
-
-        assertEquals(period.years().toSet(), setOf(2019))
-        assertTrue(period.months(2019).toSet() == (Month.FEBRUARY.value..Month.APRIL.value).toSet())
-        assert(period.monthStart(2019, Month.FEBRUARY.value) == 2)
-        assert(period.monthEnd(2019, Month.FEBRUARY.value) == 28)
-        assert(period.monthStart(2019, Month.MARCH.value) == 1)
-        assert(period.monthEnd(2019, Month.MARCH.value) == 31)
-        assert(period.monthStart(2019, Month.APRIL.value) == 1)
-        assert(period.monthEnd(2019, Month.APRIL.value) == 12)
+    fun plainYears() {
+        assertEquals(setOf(2019), plainPeriod().years().toSet())
     }
 
     @Test
-    fun crossYear() {
-        val period = ObservationPeriod(LocalDate.of(2019, Month.FEBRUARY, 2),
-                LocalDate.of(2020, Month.APRIL, 12))
-
-        assertEquals(period.years().toSet(), setOf(2019, 2020))
-        assertTrue(period.months(2019).toSet() == (Month.FEBRUARY.value..Month.DECEMBER.value).toSet())
-        assertTrue(period.months(2020).toSet() == (Month.JANUARY.value..Month.APRIL.value).toSet())
-        assert(period.monthStart(2019, Month.FEBRUARY.value) == 2)
-        assert(period.monthEnd(2019, Month.FEBRUARY.value) == 28)
-        assert(period.monthStart(2020, Month.FEBRUARY.value) == 1)
-        assert(period.monthEnd(2020, Month.FEBRUARY.value) == 29)
-        assert(period.monthStart(2020, Month.APRIL.value) == 1)
-        assert(period.monthEnd(2020, Month.APRIL.value) == 12)
+    fun plainMonths() {
+        assertEquals((Month.FEBRUARY.value..Month.APRIL.value).toSet(), plainPeriod().months(2019).toSet())
     }
+
+    @Test
+    fun wideYears() {
+        assertEquals(setOf(2019, 2020), crossYearPeriod().years().toSet())
+    }
+
+    @Test
+    fun wideMonth() {
+        with(crossYearPeriod()) {
+            assertEquals(setOf(Month.DECEMBER.value), months(2019).toSet())
+            assertEquals((Month.JANUARY.value..Month.MARCH.value).toSet(), months(2020).toSet())
+        }
+    }
+
+    @ParameterizedTest(name = "plain year period: {0}.{1} month bounds are [{2}, {3}]")
+    @CsvSource(
+            "2019, 2, 2, 28",
+            "2019, 3, 1, 31",
+            "2019, 4, 1, 12"
+    )
+    fun plainMonthBounds(year: Int, month: Int, start: Int, end: Int) {
+        with(plainPeriod()) {
+            assertEquals(monthStart(year, month), start)
+            assertEquals(monthEnd(year, month), end)
+        }
+    }
+
+    @ParameterizedTest(name = "cross year period: {0}.{1} month bounds are [{2}, {3}]")
+    @CsvSource(
+            "2019, 12, 2, 31",
+            "2020, 1, 1, 31",
+            "2020, 2, 1, 29",
+            "2020, 3, 1, 1"
+    )
+    fun wideMonthBounds(year: Int, month: Int, start: Int, end: Int) {
+        with(crossYearPeriod()) {
+            assertEquals(monthStart(year, month), start)
+            assertEquals(monthEnd(year, month), end)
+        }
+    }
+
+    @Test
+    fun length() {
+        assertEquals(plainPeriod().length(), 69)
+        assertEquals(crossYearPeriod().length(), 90)
+    }
+
+    //@ParameterizedTest
+    @CsvSource(
+            "",
+            "",
+            ""
+    )
+    fun intersects(){
+
+    }
+
+
+    private fun plainPeriod() = ObservationPeriod(
+            LocalDate.of(2019, Month.FEBRUARY, 2),
+            LocalDate.of(2019, Month.APRIL, 12))
+
+
+    private fun crossYearPeriod() = ObservationPeriod(
+            LocalDate.of(2019, Month.DECEMBER, 2),
+            LocalDate.of(2020, Month.MARCH, 1))
+
 }
