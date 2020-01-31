a84cab36001c2381b310e27289910547ef6fa9a6
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2020-01-31 21:20:04 +0300

    format kt code, fix main page

--- src/main/kotlin/ru/cleverclover/huncale/AskMe.kt
+++ src/main/kotlin/ru/cleverclover/huncale/AskMe.kt
@@ -34,14 +34,15 @@ class AskMe {
 
     @GetMapping("/calendarDataAjax")
     @ResponseBody
-    fun calendarData(@RequestParam params: Map<String, Any>) = with(ObservatoryConfig(params).observatory()) {
-        val resources = Resources(Data.targets(), scope)
-        mapOf(
-            "years" to TimeLine(scope).data(),
-            "beacons" to Beacons(this).data(),
-            "resources" to resources.data(),
-            "resourcesInScope" to resources.inScope()
-        )
-    }
+    fun calendarData(@RequestParam params: Map<String, Any>) =
+        with(ObservatoryConfig(params).observatory()) {
+            val resources = Resources(Data.targets(), scope)
+            mapOf(
+                "years" to TimeLine(scope).data(),
+                "beacons" to Beacons(this).data(),
+                "resources" to resources.data(),
+                "resourcesInScope" to resources.inScope()
+            )
+        }
 
 }
