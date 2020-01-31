2c8176f30156a4802088be303d845a4e3d68024b
elena.parovyshnaya <elena.parovyshnaya@gmail.com>
2020-01-31 16:04:55 +0300

    Provide license headers for the sources

--- src/main/kotlin/ru/cleverclover/huncale/timemachine/DateText.kt
+++ src/main/kotlin/ru/cleverclover/huncale/timemachine/DateText.kt
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2019, 2020 CleverClover
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT which is available at
+ * https://spdx.org/licenses/MIT.html#licenseText
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *     CleverClover - initial API and implementation
+ *******************************************************************************/
 package ru.cleverclover.huncale.timemachine
 
 import java.time.LocalDate
@@ -6,6 +18,7 @@ import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoField
 
 internal object DateText {
+
     private val labelFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy")
 
     fun label(date: LocalDate): String = labelFormat.format(date)
@@ -13,4 +26,5 @@ internal object DateText {
     fun date(label: String): LocalDate = with(labelFormat.parse(label)) {
         LocalDate.of(get(ChronoField.YEAR), get(ChronoField.MONTH_OF_YEAR), get(ChronoField.DAY_OF_MONTH))
     }
+
 }
